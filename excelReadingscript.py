0e7df75faa1e71ffb0b1e5c215fedbc51a446514
Joshua Ryland <joshua.ryland@ad.maynardmarks.co.nz>
2019-08-19 10:32:49 +1200

    GUI Implementation 50%

--- excelReadingscript.py
+++ excelReadingscript.py
@@ -1,36 +1,72 @@
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
-import tkinter as tk
+import PySimpleGUI as sg
 
-window = tk.Tk()
-window.geometry("540x700")
-window.title("ReInspection GUI")
+layout = [[sg.Text('Filename:')],
+            [sg.Input(), sg.FileBrowse()],
+            [sg.Submit(), sg.Cancel()],
+            [sg.Output(size=(80,20))]]
 
-tk.Label(window, text = "Excel Pathname:").grid(row = 0)
-pathname = tk.Entry(window).grid(row = 0, column = 1)
+window = sg.Window("Reinspection Wizard",layout, default_element_size=(130,250))
 
-window.mainloop()
+while True:
+    event,values = window.Read()
+    if event is None or event == "Cancel":
+        break
+    elif event is event == "Submit":
+        pathname = values[0]
+        print(pathname)
+        sheet = pd.read_excel(pathname)
+        sheet['Reinspect Date'] = pd.to_datetime(sheet['Reinspect Date'])
 
-sheet = pd.read_excel("/Users/joshua.ryland/Desktop/UNI/test.xlsx")
+        start_date = '01-01-2018'
+        end_date = '01-01-2020'
 
-print("Column headings:")
-print(sheet.columns)
+        mask = (sheet['Reinspect Date'] > start_date) & (sheet['Reinspect Date'] <= end_date)
 
-reinspectDate =  sheet['Reinspect Date']
-print(reinspectDate)
+        sheet = sheet.loc[mask]
+        print("There are a total of "+ str(len(sheet)) +" overdue items for reinspection.")
+        print("They are between the dates 01/01/2018 and 01/01/2020.")
+window.Close()
 
-sheet['Reinspect Date'] = pd.to_datetime(sheet['Reinspect Date'])
+# def ChatBot():
+#     layout = [[(sg.Text('This is where standard out is being routed', size=[40, 1]))],
+#               [sg.Output(size=(80, 20))],
+#               [sg.Multiline(size=(70, 5), enter_submits=True),
+#                sg.Button('SEND', button_color=(sg.YELLOWS[0], sg.BLUES[0])),
+#                sg.Button('EXIT', button_color=(sg.YELLOWS[0], sg.GREENS[0]))]]
 
-start_date = '01-01-2018'
-end_date = '01-01-2020'
+#     window = sg.Window('Chat Window', layout, default_element_size=(30, 2))
 
-mask = (sheet['Reinspect Date'] > start_date) & (sheet['Reinspect Date'] <= end_date)
+#     # ---===--- Loop taking in user input and using it to query HowDoI web oracle --- #
+#     while True:
+#         event, value = window.Read()
+#         if event == 'SEND':
+#             print(value)
+#         else:
+#             break
 
-sheet = sheet.loc[mask]
-print("There are a total of "+ str(len(sheet)) +" overdue items for reinspection.")
-print("They are between the dates 01/01/2018 and 01/01/2020.")
-sheet
+# ChatBot()
+# sheet = pd.read_excel("/Users/joshua.ryland/Desktop/UNI/test.xlsx")
+
+# print("Column headings:")
+# print(sheet.columns)
+
+# reinspectDate =  sheet['Reinspect Date']
+# print(reinspectDate)
+
+# sheet['Reinspect Date'] = pd.to_datetime(sheet['Reinspect Date'])
+
+# start_date = '01-01-2018'
+# end_date = '01-01-2020'
+
+# mask = (sheet['Reinspect Date'] > start_date) & (sheet['Reinspect Date'] <= end_date)
+
+# sheet = sheet.loc[mask]
+# print("There are a total of "+ str(len(sheet)) +" overdue items for reinspection.")
+# print("They are between the dates 01/01/2018 and 01/01/2020.")
+# sheet
 
 #Open GUI Window
 
