407f856b94fa6046ed9a62af4f45066ce2e92128
Joshua Ryland <joshua.ryland@ad.maynardmarks.co.nz>
2019-08-19 09:30:52 +1200

    Trust Management

--- TrustScript.py
+++ TrustScript.py
@@ -1,12 +1,18 @@
 import pandas as pd
+import sys
 from pandas import ExcelWriter
 from pandas import ExcelFile
-#import tkinter as tk
 
-sheet = pd.read_excel("K:/Jobs/T/Trust Management (TRU)/AMS_Properties/West Auckland Trust Services Limited/Master V1 West Auckland Trust Services Limited.xlsx")
 
-#sheet.dropna(inplace = True)
-
+sheet = pd.read_excel("K:/Jobs/T/Trust Management (TRU)/AMS_Properties/General Church Trust Board/Master Register V1 General Church Trust Board.xlsx")
+#West Auckland Trust Services
+#K:/Jobs/T/Trust Management (TRU)/AMS_Properties/West Auckland Trust Services Limited/Master V1 West Auckland Trust Services Limited.xlsx
+#Trust Investment Property Fund
+#K:/Jobs/T/Trust Management (TRU)/AMS_Properties/Trust Investment Property Fund/Master Register V1 Trust Investment Properry Fund.xlsx
+#St Stephens & Queen Victoria Trust Board
+#K:/Jobs/T/Trust Management (TRU)/AMS_Properties/St Stephens & Queen Victoria Trust Board/Master Register V1 St Stephens & Queen Victoria Trust Board.xlsx
+#General Trust Board
+#K:/Jobs/T/Trust Management (TRU)/AMS_Properties/General Church Trust Board/Master Register V1 General Church Trust Board.xlsx
 #print("Column headings:")
 #print(sheet.columns)
 
@@ -25,6 +31,9 @@ sheet = sheet.loc[mask]
 
 print("There are a total of "+ str(len(sheet)) +" overdue items for reinspection.")
 print("They are between the dates 01/01/2018 and 01/01/2020.")
+#Print Data Sheet if there are values of worth.
+if(len(sheet) == 0):
+    sys.exit
 sheet
 
 #Look at a gui system
