78f9959ddaff48f8c3dc9bebcd79b8b240f435f7
Joshua Ryland <joshua.ryland@ad.maynardmarks.co.nz>
2019-08-19 12:01:05 +1200

    Ease of life changes

--- excelReadingscript.py
+++ excelReadingscript.py
@@ -26,28 +26,20 @@ while True:
         mask = (sheet['Reinspect Date'] > start_date) & (sheet['Reinspect Date'] <= end_date)
 
         sheet = sheet.loc[mask]
+        if(len(sheet)==0):
+            print("")
+            print("There are no reinspections required at this time.")
+            continue
         print("There are a total of "+ str(len(sheet)) +" overdue items for reinspection.")
         print("They are between the dates 01/01/2018 and 01/01/2020.")
+        print("These are at the following properties:")
+        print("")
+        propertyDict = sheet['Property Name'].to_dict()
+        for amount, properties in propertyDict.items():
+            print(properties)
+            print("")
 window.Close()
 
-# def ChatBot():
-#     layout = [[(sg.Text('This is where standard out is being routed', size=[40, 1]))],
-#               [sg.Output(size=(80, 20))],
-#               [sg.Multiline(size=(70, 5), enter_submits=True),
-#                sg.Button('SEND', button_color=(sg.YELLOWS[0], sg.BLUES[0])),
-#                sg.Button('EXIT', button_color=(sg.YELLOWS[0], sg.GREENS[0]))]]
-
-#     window = sg.Window('Chat Window', layout, default_element_size=(30, 2))
-
-#     # ---===--- Loop taking in user input and using it to query HowDoI web oracle --- #
-#     while True:
-#         event, value = window.Read()
-#         if event == 'SEND':
-#             print(value)
-#         else:
-#             break
-
-# ChatBot()
 # sheet = pd.read_excel("/Users/joshua.ryland/Desktop/UNI/test.xlsx")
 
 # print("Column headings:")
