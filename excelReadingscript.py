936ad3d82a8b220e6b9101e8a743f46875f24601
Joshua Ryland <joshua.ryland@ad.maynardmarks.co.nz>
2019-08-22 10:50:50 +1200

    test

--- excelReadingscript.py
+++ excelReadingscript.py
@@ -18,6 +18,7 @@ while True:
         pathname = values[0]
         print(pathname)
         sheet = pd.read_excel(pathname)
+    
         sheet['Reinspect Date'] = pd.to_datetime(sheet['Reinspect Date'])
 
         start_date = '01-01-2018'
@@ -39,26 +40,3 @@ while True:
             print(properties)
             print("")
 window.Close()
-
-# sheet = pd.read_excel("/Users/joshua.ryland/Desktop/UNI/test.xlsx")
-
-# print("Column headings:")
-# print(sheet.columns)
-
-# reinspectDate =  sheet['Reinspect Date']
-# print(reinspectDate)
-
-# sheet['Reinspect Date'] = pd.to_datetime(sheet['Reinspect Date'])
-
-# start_date = '01-01-2018'
-# end_date = '01-01-2020'
-
-# mask = (sheet['Reinspect Date'] > start_date) & (sheet['Reinspect Date'] <= end_date)
-
-# sheet = sheet.loc[mask]
-# print("There are a total of "+ str(len(sheet)) +" overdue items for reinspection.")
-# print("They are between the dates 01/01/2018 and 01/01/2020.")
-# sheet
-
-#Open GUI Window
-
